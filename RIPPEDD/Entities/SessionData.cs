6d6501796c89959955e913719e0b5446a245f440
Chris Hamilton <cohamilt@asu.edu>
2014-03-29 14:32:06 -0700

    Rework for session variable

---
+++ RIPPEDD/Entities/SessionData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RIPPEDD.Entities
+{
+    public class SessionData
+    {
+        public int _loginID { get; set; }
+        public int _userType { get; set; }
+
+    }
+}
