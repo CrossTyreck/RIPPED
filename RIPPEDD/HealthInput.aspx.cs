c67d1c0f0d096925ecaa36089d43666351e10a35
nathanielmendoza <namendoz@asu.edu>
2014-04-19 17:34:24 -0700

    + HealthInput.aspx now redirects to Login if there is no Session ID

--- RIPPEDD/HealthInput.aspx.cs
+++ RIPPEDD/HealthInput.aspx.cs
@@ -24,8 +24,16 @@ namespace RIPPEDD
             WorkoutChoices_Initialize();
 
           //Needs to be in the login function
-            
-             userID = ((SessionData)Session["User_Data"])._loginID;
+
+            try
+            {
+                userID = ((SessionData)Session["User_Data"])._loginID;
+            }
+            catch (NullReferenceException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                Response.Redirect("Login.aspx");
+            }
             
         }
 
