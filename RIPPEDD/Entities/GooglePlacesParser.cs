a144b73e1f637469358baa35ce2bd2fdb4b8e060
smishr16 <sitanshu.mishra@asu.edu>
2014-04-29 14:54:52 -0700

    Parser class for Google Places

---
+++ RIPPEDD/Entities/GooglePlacesParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RIPPEDD.Entities
+{
+    public class GooglePlacesParser
+    {
+        public string status { get; set; }
+        public results[] results { get; set; }
+    }
+    public class results
+    {
+        public geometry geometry { get; set; }
+        public string name { get; set; }
+        public string formatted_address { get; set; }
+        //public string reference { get; set; }
+        //public string vicinity { get; set; }
+    }
+    public class geometry
+    {
+        public location location { get; set; }
+    }
+    public class location
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+}
